b2075401518f98557807ba199bfd2434cd05927d
Felipe Alexandre <felipealexandre98@gmail.com>
2025-09-09 22:13:56 -0300

    Corrigir componentes para celular

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -95,7 +95,7 @@ const Hero = () => {
       </div>
 
       {/* Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce hidden md:block">
         <div className="w-6 h-10 border-2 border-background/50 rounded-full flex justify-center">
           <div className="w-1 h-3 bg-background/70 rounded-full mt-2"></div>
         </div>
